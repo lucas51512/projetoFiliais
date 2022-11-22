450b7d5f27c0069d991d4c58c044bfeb18110823
Lucas <56892062+lucas51512@users.noreply.github.com>
2022-11-22 10:50:39 -0300

    Delete projetofiliais directory

--- projetofiliais/projetofiliais/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace projetofiliais.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
